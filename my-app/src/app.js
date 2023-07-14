7bfa88ca6a814c090a0ee95eec0708d48ad1c371
Mir2226 <gmihran@internet.ru>
2023-07-14 17:17:28 +0400

    created add, delete , change, update functions for toDoList

---
+++ my-app/src/app.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { fontAwesomeicon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faPen, faTrashcan } from '@fortawesome/free-solid-svg-icons';
+import './app.css';
+
+function App() {
+    
+    // Tasks todo list state
+    const [toDo, setToDo] = useState([
+        {'id': 1, 'title': 'task 1', 'status' : false},
+        {'id': 2, 'title': 'task 2', 'status' : false}
+    ]);
+
+
+
+
+    // Temp state
+    const [newTask, setNewTask] = useState('');
+    const [updateData, setUpdateData] = useState('');
+    
+    // Add task
+    const addTask = () => {
+
+    }
+    
+    // Delete task
+    const deleteTask = (id) => {
+
+    }
+
+    // Mark task as done or completed
+    const markDone = (id) => {
+
+    }
+    
+    
+    // Cancel update
+    const cancelUpdate = (id) => {
+
+    }
+
+     // Change task for update
+     const changeTask = () => {
+
+     }
+    
+    
+    
+     return (
+        <div className=" container App">
+           <br></br>
+           <h2>To Do List App</h2>
+        </div>
+    )
+}
+
+export default App;
